ac59598ac32a14a7ab251af916895aa0840b6368
nohha0 <151031005+nohhayeong@users.noreply.github.com>
2024-02-06 09:27:43 +0900

    init

---
+++ src/main/kotlin/com/example/novelnook/NovelNookApplication.kt
@@ -0,0 +1,11 @@
+package com.example.novelnook
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NovelNookApplication
+
+fun main(args: Array<String>) {
+    runApplication<NovelNookApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/novelnook/NovelNookApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.novelnook
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NovelNookApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
