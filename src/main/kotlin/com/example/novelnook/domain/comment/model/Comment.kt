e6b68da9ce5b18615c6285d7da2b9b2150392a95
nohha0 <151031005+nohhayeong@users.noreply.github.com>
2024-02-08 09:55:33 +0900

    add comment entity

--- src/main/kotlin/com/example/novelnook/domain/comment/model/Comment.kt
+++ src/main/kotlin/com/example/novelnook/domain/comment/model/Comment.kt
@@ -1,4 +1,18 @@
 package com.example.novelnook.domain.comment.model
 
-class Comment {
+import jakarta.persistence.*
+import java.time.LocalDateTime
+
+@Entity
+@Table(name = "comment")
+class Comment (
+    @Column
+    var content:String
+){
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long? = null
+
+    @Column(updatable = false)
+    val createdAt: LocalDateTime = LocalDateTime.now()
 }
