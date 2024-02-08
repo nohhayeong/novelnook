43b763996d25c5a29539f710275648914f7cb10e
nohha0 <151031005+nohhayeong@users.noreply.github.com>
2024-02-08 09:52:54 +0900

    add post entity

--- src/main/kotlin/com/example/novelnook/domain/post/model/Post.kt
+++ src/main/kotlin/com/example/novelnook/domain/post/model/Post.kt
@@ -1,4 +1,21 @@
 package com.example.novelnook.domain.post.model
 
-class Post {
+import jakarta.persistence.*
+import java.time.LocalDateTime
+
+@Entity
+@Table(name = "post")
+class Post (
+    @Column
+    var title:String,
+
+    @Column
+    var content:String,
+){
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id:Long?=null
+
+    @Column(updatable = false)
+    val createdAt:LocalDateTime = LocalDateTime.now()
 }
